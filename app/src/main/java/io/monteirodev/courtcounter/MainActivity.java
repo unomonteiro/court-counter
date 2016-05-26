31bb515503476f5b60f55d030872f3d19eb75ef3
Carlos Monteiro <unomonteiro@gmail.com>
2016-05-26 21:20:51 +0100

    extra udacity course: saving scores even after onDestroy.

--- app/src/main/java/io/monteirodev/courtcounter/MainActivity.java
+++ app/src/main/java/io/monteirodev/courtcounter/MainActivity.java
@@ -1,23 +1,70 @@
 package io.monteirodev.courtcounter;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    int scoreTeamA = 0;
-    int scoreTeamB = 0;
+    private static final String TAG = MainActivity.class.getSimpleName();
+    public static final String PREFS_FILE = "io.monteirodev.courtcounter.preferences";
+    public static final String KEY_SCORE_TEAM_A = "score_team_a";
+    public static final String KEY_SCORE_TEAM_B = "score_team_b";
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
+
+    private int scoreTeamA;
+    private int scoreTeamB;
+
+//    // Keeps scores on rotation, but looses onDestroy()
+//    @Override
+//    protected void onSaveInstanceState(Bundle outState) {
+//        super.onSaveInstanceState(outState);
+//
+//        outState.putInt(KEY_SCORE_TEAM_A, scoreTeamA);
+//        outState.putInt(KEY_SCORE_TEAM_B, scoreTeamB);
+//
+//        Log.d(TAG, "onSaveInstanceState A: " + scoreTeamA);
+//    }
+//
+//    @Override
+//    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+//        super.onRestoreInstanceState(savedInstanceState);
+//        scoreTeamA = savedInstanceState.getInt(KEY_SCORE_TEAM_A);
+//        scoreTeamB = savedInstanceState.getInt(KEY_SCORE_TEAM_B);
+//        Log.d(TAG, "onRestoreInstanceState A: " + scoreTeamA);
+//        displayForTeamA(scoreTeamA);
+//        displayForTeamB(scoreTeamB);
+//    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        displayForTeamA(0);
-        displayForTeamB(0);
 
-        // todo activityLifecycle
-        // todo savedPreferences
+        mSharedPreferences = getSharedPreferences(PREFS_FILE, Context.MODE_PRIVATE);
+        mEditor = mSharedPreferences.edit();
+        scoreTeamA = mSharedPreferences.getInt(KEY_SCORE_TEAM_A, 0);
+        scoreTeamB = mSharedPreferences.getInt(KEY_SCORE_TEAM_B, 0);
+        Log.d(TAG, "onCreate A: " + scoreTeamA);
+
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        mEditor.putInt(KEY_SCORE_TEAM_A, scoreTeamA);
+        mEditor.putInt(KEY_SCORE_TEAM_B, scoreTeamB);
+        mEditor.apply();
+        Log.d(TAG, "onPause A: " + scoreTeamA);
     }
 
     public void addThreeForTeamA(View view) {
@@ -51,7 +98,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void resetScore(View view) {
-        scoreTeamA = scoreTeamB = 0;
+//        mEditor.clear();
+//        mEditor.apply();
+        scoreTeamA = 0;
+        scoreTeamB = 0;
         displayForTeamA(scoreTeamA);
         displayForTeamB(scoreTeamB);
     }
